bd231fc529f8ca5929f4c24b1085f8a08d336b6f
Jacob Alheid <shakefu@gmail.com>
2022-12-06 07:55:46 +0000

    fix(setup): indent

--- setup.py-example/setup.py
+++ setup.py-example/setup.py
@@ -2,17 +2,18 @@
 
 from setuptools import setup, find_packages
 
-setup(name='Hello',
-      version='1.0',
-      description='Example package',
-      author='Me',
-      author_email='shakefu@gmail.com',
-      packages=find_packages(),
-      install_requires=[
-          "requests"
-          ],
-      test_suite='nose.collector',
-      tests_require=[
-          "nose",
-          ]
-     )
+setup(
+    name='Hello',
+    version='1.0',
+    description='Example package',
+    author='Me',
+    author_email='shakefu@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        "requests"
+        ],
+    test_suite='nose.collector',
+    tests_require=[
+        "nose",
+        ]
+)
